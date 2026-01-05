adb7cd86093163852747047b995e37f06f42fc07
ChaSoojin <soojin3233@gmail.com>
2026-01-05 16:40:38 +0900

    백+프론트 전체 셋팅 작업

--- src/main/java/com/aivle0102/bigproject/BigProjectApplication.java
+++ src/main/java/com/aivle0102/bigproject/BigProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.bigproject;
+package com.aivle0102.bigproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/aivle0102/bigproject/BigProjectApplicationTests.java
+++ src/test/java/com/aivle0102/bigproject/BigProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bigproject;
+package com.aivle0102.bigproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
